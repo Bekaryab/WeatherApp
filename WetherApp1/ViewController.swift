a9d0cf4876de1a6363ed5f80edcf257886a31c9b
Bekarys <abievb91@gmail.com>
2026-03-17 17:40:19 +0500

    Initial Commit

---
+++ WetherApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WetherApp1
+//
+//  Created by bekarys abiev on 17.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WetherApp1Tests/WetherApp1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WetherApp1Tests.swift
+//  WetherApp1Tests
+//
+//  Created by bekarys abiev on 17.03.2026.
+//
+
+import Testing
+@testable import WetherApp1
+
+struct WetherApp1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
